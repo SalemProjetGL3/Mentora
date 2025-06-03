119d8eb6a1d967680b9c3da1936b2b96423e3d82
issrabrini <isra.brini@insat.ucar.tn>
2025-06-03 12:39:20 +0100

    refactor(user)

--- backend/services/user-service/src/user/dto/update-user.input.ts
+++ backend/services/user-service/src/user/dto/update-user.input.ts
@@ -31,9 +31,7 @@ export class UpdateUserInput {
 
   @Field({ nullable: true })
   role?: string;
-
-  @Field({ nullable: true })          
-  isVerified?: boolean;                
+              
 
   @Field({ nullable: true })        
   verificationToken?: string;        
